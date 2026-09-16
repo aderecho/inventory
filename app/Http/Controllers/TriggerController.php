a69deeab696857b7138dcea821c4892c9f7a7178
jerecho282002 <jelatosa@up.edu.ph>
2026-09-16 15:03:57 +0800

    add body data

--- app/Http/Controllers/TriggerController.php
+++ app/Http/Controllers/TriggerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Services\TriggerService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Throwable;
 
 class TriggerController extends Controller
@@ -12,15 +13,18 @@ class TriggerController extends Controller
         protected TriggerService $triggerService
     ) {}
 
-    public function store(): JsonResponse
+    public function store(Request $request): JsonResponse
     {
         try {
+            $requestData = $request->all();
+
             $trigger = $this->triggerService->sync();
 
             return response()->json([
                 'success' => true,
                 'message' => 'Employee synchronization completed successfully.',
                 'data' => $trigger,
+                'request_data' => $requestData,
             ], 201);
 
         } catch (Throwable $e) {
